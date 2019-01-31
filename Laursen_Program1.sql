271c5dd6296ddc0e67fd585addeac75f8dde3729
Julie Laursen <julielaursen1@gmail.com>
2019-01-30 19:19:42 -0600

    Program 1

---
+++ Laursen_Program1.sql
@@ -0,0 +1,15 @@
+
+
+DECLARE
+
+   lv_first_block VARCHAR2(50) := 'This is my first PL/SQL Block';
+   lv_second_block VARCHAR2(50) := 'Today''s date is ';
+   lv_date DATE := SYSDATE;
+   
+BEGIN
+
+   DBMS_OUTPUT.PUT_LINE(lv_first_block);
+   DBMS_OUTPUT.PUT_LINE(lv_second_block || lv_date);
+    
+END;
+/
