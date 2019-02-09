5510aff7fbc2e3ec44b847c484872e897d2e6afb
Julie Laursen <julielaursen1@gmail.com>
2019-02-09 15:13:59 -0600

    "fixed error"

--- Laursen_Program4.sql
+++ Laursen_Program4.sql
@@ -1,3 +1,21 @@
+--/***************************************************************/
+--  Developer:            Julie Laursen
+--
+--  Program #:            3
+--
+--  File Name:            Program 3.sql
+--
+--  Course:               ITSE 1345 Introduction to Oracle SQL and PL/SQL
+--
+--  Due Date:             <Due Date>
+--
+--  Instructor:           Fred Kumi 
+--
+--  Chapter:              4
+--
+--  Description:
+--     Calculate customer rating based on total purchases and display on the screen
+--
 
 set Serveroutput On;
 
@@ -18,6 +36,18 @@ lv_rating := 'test';
 	ELSE
 		lv_rating := 'low';
 	END IF;
-		DBMS_OUPUT.PUT_LINE(lv_rating);
+		DBMS_OUTPUT.PUT_LINE(lv_rating);
+		
+	CASE  
+		WHEN lv_purchase > 500.00 THEN
+			lv_rating := 'high';
+		WHEN lv_purchase > 350.00 THEN
+			lv_rating := 'medium';
+		WHEN lv_purchase > 250.00 THEN
+			lv_rating := 'normal';
+		ELSE
+			lv_rating := 'low';
+		END CASE;
+		DBMS_OUTPUT.PUT_LINE(lv_rating);
 END;
 /
