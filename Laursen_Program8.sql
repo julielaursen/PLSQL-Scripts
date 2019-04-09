a904b9fbea3c272678670d8ee35a120aef3b2b13
Julie Laursen <julielaursen1@gmail.com>
2019-04-08 19:13:00 -0500

    draft of procedure with cursor, operators

--- Laursen_Program8.sql
+++ Laursen_Program8.sql
@@ -1,18 +1,37 @@
-
+--/***************************************************************/
+--  Developer:            Julie Laursen
+--
+--  Program #:            8
+--
+--  File Name:            Program 8.sql
+--
+--  Course:               ITSE 1345 Introduction to Oracle SQL and PL/SQL
+--
+--  Due Date:             3/15
+--
+--  Instructor:           Fred Kumi 
+--
+--  Chapter:              
+--
+--  Description:
+--     Create a procedure named DDCK_SP that confirms whether a monthly pledge amount is the corect amount
+--
 set Serveroutput On;
 
-CREATE OR REPLACE PROCEDURE DDCK_SP (pledge_id IN NUMBER, payment_amt OUT NUMBER) 
+CREATE OR REPLACE PROCEDURE DDCK_SP (pledge_id IN NUMBER, payment_amt IN NUMBER) --DD_Pledge.Pledgeamt%TYPE) 
 IS
 	CURSOR cur_pledge IS
-		SELECT Pledgeamt 
+		SELECT Pledgeamt, Paymonths 
 		FROM DD_Pledge 
 		WHERE idPledge = pledge_id;
+
 BEGIN
 	FOR rec_purch IN cur_pledge LOOP
-		IF rec_purch.Pledgeamt = payment_amt THEN
+		DBMS_OUTPUT.put_line('Pledge amount / monthly payments is' || rec_purch.Pledgeamt / rec_purch.Paymonths);
+		IF (rec_purch.Pledgeamt / rec_purch.Paymonths) = payment_amt THEN
 			DBMS_OUTPUT.put_line('The amount entered is correct');
 		ELSE
-			RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount -- planned payment is ' || rec_purch.Pledgeamt || ' should be raised. The ' ||
+			RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount -- planned payment is ' || (rec_purch.Pledgeamt / rec_purch.Paymonths) || ' should be raised. The ' ||
 			payment_amt || ' should be replaced by the correct payment amount');
 	
 	END IF;
