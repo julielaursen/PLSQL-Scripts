910496e9aedd33457fc9db8b8681e4a3dc7429af
Julie Laursen <julielaursen1@gmail.com>
2019-04-10 18:21:59 -0500

    Finished procedure and exception

--- Laursen_Program8.sql
+++ Laursen_Program8.sql
@@ -16,6 +16,8 @@
 --  Description:
 --     Create a procedure named DDCK_SP that confirms whether a monthly pledge amount is the corect amount
 --
+
+
 set Serveroutput On;
 
 CREATE OR REPLACE PROCEDURE DDCK_SP (pledge_id IN NUMBER, payment_amt IN NUMBER) --DD_Pledge.Pledgeamt%TYPE) 
@@ -24,20 +26,32 @@ IS
 		SELECT Pledgeamt, Paymonths 
 		FROM DD_Pledge 
 		WHERE idPledge = pledge_id;
+	lv_count number;
 
 BEGIN
+	--open cur_pledge;
+	lv_count := 0;
+	
 	FOR rec_purch IN cur_pledge LOOP
-		DBMS_OUTPUT.put_line('Pledge amount / monthly payments is' || rec_purch.Pledgeamt / rec_purch.Paymonths);
-		IF (rec_purch.Pledgeamt / rec_purch.Paymonths) = payment_amt THEN
+		lv_count := lv_count + 1;
+		IF rec_purch.Paymonths = 0 THEN	
+			IF rec_purch.Pledgeamt = payment_amt THEN
+				DBMS_OUTPUT.put_line('The amount entered is correct');
+			ELSE
+				RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount -- planned payment ' || rec_purch.Pledgeamt || ' should be raised. The ' ||
+				payment_amt || ' should be replaced by the correct payment amount');	
+			END IF;
+		ELSIF (rec_purch.Pledgeamt / rec_purch.Paymonths) = payment_amt THEN	
 			DBMS_OUTPUT.put_line('The amount entered is correct');
 		ELSE
-			RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount -- planned payment is ' || (rec_purch.Pledgeamt / rec_purch.Paymonths) || ' should be raised. The ' ||
-			payment_amt || ' should be replaced by the correct payment amount');
-	
+			RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount -- planned payment ' || (rec_purch.Pledgeamt / rec_purch.Paymonths) || ' should be raised. The ' ||
+			payment_amt || ' should be replaced by the correct payment amount');	
 	END IF;
 	END LOOP;
   
-EXCEPTION	
-	WHEN NO_DATA_FOUND THEN DBMS_OUTPUT.PUT_LINE('No payment information found');
+    if (lv_count = 0) then
+		RAISE_APPLICATION_ERROR(-20050, 'No payment information found');
+	END IF;
+	
 END DDCK_SP;
 /
