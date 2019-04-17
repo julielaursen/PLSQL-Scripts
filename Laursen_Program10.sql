20b99506ed4152a2ee55c907b3e720b413bbedd1
Julie Laursen <julielaursen1@gmail.com>
2019-04-17 18:48:14 -0500

    Add files via upload

--- Laursen_Program10.sql
+++ Laursen_Program10.sql
@@ -1,40 +1,32 @@
---query the BB_SHOPPER table for the following: First name, Last name, City, State, Phone, and e-mail address. The first procedure
---must use the Shopper ID to search for the record in the table. The second procedure
---must use the last name for the search. Do not print any data in the procedure. Use
---parameters to return the data to the calling program unit. Include a NO_DATA_FOUND
---exception handler to display a message that no record was found.
-
---package specification
-CREATE OR REPLACE PACKAGE shopper_pkg 
-IS
+--Not done yet, include a NO DATA FOUND EXCEPTION
+CREATE OR REPLACE PACKAGE shopper_pkg IS
     PROCEDURE shopper_search_pp 
 		(p_ShopperID IN bb_shopper.idshopper%TYPE,
-		 p_firstname IN bb_shopper.firstname%TYPE,
-		 p_lastname IN bb_shopper.lastname%TYPE,
-		 p_city IN bb_shopper.city%TYPE,
-		 p_st IN bb_shopper.st%TYPE,
-		 p_phone IN bb_shopper.phone%TYPE
-		 p_email IN bb_shopper.email%TYPE);
+		 p_firstname OUT bb_shopper.firstname%TYPE,
+		 p_lastname OUT bb_shopper.lastname%TYPE,
+		 p_city OUT bb_shopper.city%TYPE,
+		 p_st OUT bb_shopper.st%TYPE,
+		 p_phone OUT bb_shopper.phone%TYPE,
+		 p_email OUT bb_shopper.email%TYPE);
 	PROCEDURE shopper_search_pp 
-		(p_lastame IN bb_shopper.lastame%TYPE
-		p_firstname IN bb_shopper.firstname%TYPE,
-		 p_lastname IN bb_shopper.lastname%TYPE,
-		 p_city IN bb_shopper.city%TYPE,
-		 p_st IN bb_shopper.st%TYPE,
-		 p_phone IN bb_shopper.phone%TYPE
-		 p_email IN bb_shopper.email%TYPE);
+		(p_lastname IN bb_shopper.lastame%TYPE
+		 p_firstname OUT bb_shopper.firstname%TYPE,
+		 p_city OUT bb_shopper.city%TYPE,
+		 p_st OUT bb_shopper.st%TYPE,
+		 p_phone OUT bb_shopper.phone%TYPE,
+		 p_email OUT bb_shopper.email%TYPE);
 END;
-
+/
 CREATE OR REPLACE PACKAGE BODY shopper_pkg 
 	IS
 	PROCEDURE shopper_search_pp  
 		(p_ShopperID IN bb_shopper.idshopper%TYPE,
-		 p_firstname IN bb_shopper.firstname%TYPE,
-		 p_lastname IN bb_shopper.lastname%TYPE,
-		 p_city IN bb_shopper.city%TYPE,
-		 p_st IN bb_shopper.st%TYPE,
-		 p_phone IN bb_shopper.phone%TYPE
-		 p_email IN bb_shopper.email%TYPE);
+		 p_firstname OUT bb_shopper.firstname%TYPE,
+		 p_lastname OUT bb_shopper.lastname%TYPE,
+		 p_city OUT bb_shopper.city%TYPE,
+		 p_st OUT bb_shopper.st%TYPE,
+		 p_phone OUT bb_shopper.phone%TYPE,
+		 p_email OUT bb_shopper.email%TYPE);
 		IS
 		BEGIN
 			SELECT firstname, lastname, city, st, phone, email
@@ -43,13 +35,12 @@ CREATE OR REPLACE PACKAGE BODY shopper_pkg
 				WHERE idshopper = p_ShopperID;
 		END;
 	PROCEDURE shopper_search_pp  
-		(p_lastname IN bb_shopper.lastame%TYPE,
-		 p_firstname IN bb_shopper.firstname%TYPE,
-		 p_lastname IN bb_shopper.lastname%TYPE,
-		 p_city IN bb_shopper.city%TYPE,
-		 p_st IN bb_shopper.st%TYPE,
-		 p_phone IN bb_shopper.phone%TYPE
-		 p_email IN bb_shopper.email%TYPE);
+		(p_lastname IN bb_shopper.lastname%TYPE,
+		 p_firstname OUT bb_shopper.firstname%TYPE,
+		 p_city OUT bb_shopper.city%TYPE,
+		 p_st OUT bb_shopper.st%TYPE,
+		 p_phone OUT bb_shopper.phone%TYPE,
+		 p_email OUT bb_shopper.email%TYPE);
 		IS
 		BEGIN
 			SELECT firstname, lastname, city, st, phone, email
@@ -58,3 +49,4 @@ CREATE OR REPLACE PACKAGE BODY shopper_pkg
 				WHERE lastname = p_lastname;
 		END;
 	END;
+	/
