55c6fdcae73f08c3a020a3da95d5dcb1101e17c0
Julie Laursen <julielaursen1@gmail.com>
2019-04-17 18:33:00 -0500

    Add files via upload

--- Laursen_Program10.sql
+++ Laursen_Program10.sql
@@ -5,28 +5,55 @@
 --exception handler to display a message that no record was found.
 
 --package specification
-CREATE OR REPLACE PACKAGE myPackage
+CREATE OR REPLACE PACKAGE shopper_pkg 
 IS
-    PROCEDURE proc1 (p_ShopperID IN bb_shopper.idshopper%TYPE);
-	PROCEDURE proc2 (p_lastame IN bb_shopper.lastame%TYPE);
+    PROCEDURE shopper_search_pp 
+		(p_ShopperID IN bb_shopper.idshopper%TYPE,
+		 p_firstname IN bb_shopper.firstname%TYPE,
+		 p_lastname IN bb_shopper.lastname%TYPE,
+		 p_city IN bb_shopper.city%TYPE,
+		 p_st IN bb_shopper.st%TYPE,
+		 p_phone IN bb_shopper.phone%TYPE
+		 p_email IN bb_shopper.email%TYPE);
+	PROCEDURE shopper_search_pp 
+		(p_lastame IN bb_shopper.lastame%TYPE
+		p_firstname IN bb_shopper.firstname%TYPE,
+		 p_lastname IN bb_shopper.lastname%TYPE,
+		 p_city IN bb_shopper.city%TYPE,
+		 p_st IN bb_shopper.st%TYPE,
+		 p_phone IN bb_shopper.phone%TYPE
+		 p_email IN bb_shopper.email%TYPE);
 END;
-/
 
 CREATE OR REPLACE PACKAGE BODY shopper_pkg 
 	IS
-	PROCEDURE search_Shopper_pp (p_ShopperID)
+	PROCEDURE shopper_search_pp  
+		(p_ShopperID IN bb_shopper.idshopper%TYPE,
+		 p_firstname IN bb_shopper.firstname%TYPE,
+		 p_lastname IN bb_shopper.lastname%TYPE,
+		 p_city IN bb_shopper.city%TYPE,
+		 p_st IN bb_shopper.st%TYPE,
+		 p_phone IN bb_shopper.phone%TYPE
+		 p_email IN bb_shopper.email%TYPE);
 		IS
 		BEGIN
 			SELECT firstname, lastname, city, st, phone, email
-				into  
+				into  p_firstname, p_lastame, p_city, p_st, p_phone, p_email
 				FROM BB_SHOPPER
 				WHERE idshopper = p_ShopperID;
 		END;
-	PROCEDURE search_Shopper (p_lastname)
+	PROCEDURE shopper_search_pp  
+		(p_lastname IN bb_shopper.lastame%TYPE,
+		 p_firstname IN bb_shopper.firstname%TYPE,
+		 p_lastname IN bb_shopper.lastname%TYPE,
+		 p_city IN bb_shopper.city%TYPE,
+		 p_st IN bb_shopper.st%TYPE,
+		 p_phone IN bb_shopper.phone%TYPE
+		 p_email IN bb_shopper.email%TYPE);
 		IS
 		BEGIN
 			SELECT firstname, lastname, city, st, phone, email
-				into  
+				into  p_firstname, p_lastame, p_city, p_st, p_phone, p_email 
 				FROM BB_SHOPPER
 				WHERE lastname = p_lastname;
 		END;
