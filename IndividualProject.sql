b1207b08753e07d260f94997c9ed8ca628439e35
Julie Laursen <julielaursen1@gmail.com>
2019-05-07 22:06:10 -0500

    Update IndividualProject.sql

--- IndividualProject.sql
+++ IndividualProject.sql
@@ -2,7 +2,7 @@
 --lowest fine amount assigned to a crime charged, etc.
 --14 days between crime and hearing date
 --all criminals with crime charged
-
+--officers that reported more than an avg number of crimes
 1)
 SELECT o.last, c.crime_id FROM officers o, crimes c, crime_officers co WHERE c.crime_id = co.crime_id and o.officer_id = co.officer_id;
 then select AVG(column) FROM table_name;
@@ -13,6 +13,11 @@ select min(fine_amount) from crime_charges;
 4)
 SELECT c.last, cr.crime_id, cc.charge_id FROM criminals c, crimes cr, crime_charges cc WHERE c.criminal_id = cr.criminal_id AND cr.crime_id = cc.crime_id;	
 	
+5)
+select o.last, o. first, c.crime_id from officers o, crime_officer co, crimes c where crimes > avg
+
+6)
+select c.last, c.first from criminals c, crimes cr where c.criminal_id = cr.criminal_id and crimes > avg
 	--PROCEDURE city_jail_sp
 		--(p_query IN whatever_table_name NUMBER 
 --	 
