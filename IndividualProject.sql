61da1af58bbc4f9c09880d6d60d39b27888346fa
Julie Laursen <julielaursen1@gmail.com>
2019-05-09 15:20:20 -0500

    Update IndividualProject.sql

--- IndividualProject.sql
+++ IndividualProject.sql
@@ -2,27 +2,23 @@
 --lowest fine amount assigned to a crime charged, etc.
 --14 days between crime and hearing date
 --all criminals with crime charged
---officers that reported more than an avg number of crimes
+--full name of officers who've reported more than the average crimes charged
+--full name of criminals whove reported less than the average crimes charged
+
 1)
-SELECT o.last, c.crime_id FROM officers o, crimes c, crime_officers co WHERE c.crime_id = co.crime_id and o.officer_id = co.officer_id;
-then select AVG(column) FROM table_name;
-2)
-select min(fine_amount) from crime_charges;
+SELECT AVG(COUNT(*)) FROM crime_officers GROUP BY officer_id;
+2) 
+SELECT min(fine_amount) FROM crime_charges;
 3)
-select cr.crime_ID, cr.classification, cr.date_charged, cr.hearing date, and number of days from crimes cr
-between the date charged and the hearing date.
-
+ SELECT crime_id, classification, date_charged, Hearing_date, (hearing_date - date_charged) as days FROM crimes WHERE (hearing_date - date_charged) > 14;
 4)
-SELECT c.last, cr.crime_id, cc.charge_id FROM criminals c, crimes cr, crime_charges cc WHERE c.criminal_id = cr.criminal_id AND cr.crime_id = cc.crime_id;	
-	
+
+SELECT c.criminal_id, cr.last, cr.first, cc.crime_code, cc.fine_amount FROM crimes c, criminals cr, crime_charges cc WHERE c.criminal_id = cr.criminal_id and c.crime_id = cc.crime_id;
+
 5)
-select o.last, o. first, c.crime_id from officers o, crime_officer co, crimes c where crimes > avg
-
+ SELECT o.last, o.first FROM officers o, crime_officers co, crimes c WHERE c.crime_id = co.crime_id AND o.officer_id = co.officer_id GROUP BY o.last, o.first HAVING COUNT(*)> (SELECT AVG(COUNT(*)) FROM crime_officers GROUP BY officer_id);
 6)
-select c.last, c.first from criminals c, crimes cr where c.criminal_id = cr.criminal_id and cr.crimes > (SELECT AVG(crimes) FROM *)
-
-
-	--PROCEDURE city_jail_sp
+SELECT c.last, c.first FROM criminals c, crimes cr WHERE c.criminal_id = cr.criminal_id GROUP BY c.last, c.first HAVING COUNT(*) < (SELECT AVG(COUNT(*)) FROM crimes GROUP BY criminal_id);	--PROCEDURE city_jail_sp
 		--(p_query IN whatever_table_name NUMBER 
 --	 
 		 
